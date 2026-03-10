e090c75fcfde56c49c015d740e277d045dbbc352
YaelCastelo <julio.castelo270254@potros.itson.edu.mx>
2026-03-10 09:45:25 -0700

    Add Main_seleccion class to demonstrate selection sort

---
+++ Ordenamientoss/Main_seleccion.java
@@ -0,0 +1,12 @@
+public class Main_seleccion {
+
+    public static void main(String[] args) {
+
+        int[] numeros = {5, 3, 8, 1}; // se crea un arreglo desordenado
+        Ordenamiento_seleccion.ordenar(numeros); // se llama al método para ordenar el arreglo
+        for (int n : numeros) { // ciclo que recorre cada número del arreglo
+            System.out.print(n + " "); // imprime cada número ordenado en la consola
+        }
+    }
+
+}
