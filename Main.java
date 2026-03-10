ecd97defba13a80e698e30cda032d22a14ee00a8
YaelCastelo <julio.castelo270254@potros.itson.edu.mx>
2026-03-09 19:28:40 -0700

    Main

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Programa iniciado");
+
+        //llamar funcionalidades
+        Ordenamientoss orden = new Ordenamientoss();
+        orden.Ordenamiento_burbuja();
+
+    }
+}
